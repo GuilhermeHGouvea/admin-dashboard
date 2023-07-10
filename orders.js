d8597389ba42f2b1215032b5fc7017f250210d5a
Guilherme Henrique <guilhermehgoueva@gmail.com>
2023-07-09 21:01:07 -0300

    first-commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'FullStack JavaScript',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Curso Completo CSS',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Tutorial Flex-Box',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
